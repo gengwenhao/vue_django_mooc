4b2f76b919a0d904f9fea69a01682fdd9b188a28
gengwenhao <gengwenhao97@126.com>
2020-02-07 20:36:59 +0800

    加入了课程爬虫

--- django_mooc/django_mooc/apps/banner/models.py
+++ django_mooc/django_mooc/apps/banner/models.py
@@ -10,8 +10,10 @@ class BannerModel(models.Model):
     title = models.CharField(max_length=32, verbose_name='标题')
     sub_title = models.CharField(max_length=32, null=True, blank=True, verbose_name='副标题')
     index = models.PositiveSmallIntegerField(default=1, verbose_name='轮播图序号')
-    course = models.ForeignKey(CourseModel, null=True, blank=True, on_delete=models.DO_NOTHING)
-    img = models.ImageField(upload_to='banners/%Y/%m/%d', default='default/banner.png', verbose_name='轮播图片')
+    course = models.ForeignKey(CourseModel, null=True, blank=True, on_delete=models.DO_NOTHING,
+                               verbose_name='关联课程')
+    img = models.ImageField(upload_to='banners/%Y/%m/%d', default='default/banner.png',
+                            verbose_name='图片（建议尺寸960*425）')
 
     class Meta:
         db_table = 'geng_banner'
