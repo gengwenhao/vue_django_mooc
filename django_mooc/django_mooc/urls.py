3be7a577673c6ccf64cc624beb8f35629a68e45d
gengwenhao <gengwenhao97@126.com>
2020-02-22 19:18:37 +0800

    编译前端放入django项目的static目录中

--- django_mooc/django_mooc/urls.py
+++ django_mooc/django_mooc/urls.py
@@ -37,5 +37,5 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
-    # urlpatterns.insert(0, path('', TemplateView.as_view(template_name='index.html')))
+    urlpatterns.insert(0, path('', TemplateView.as_view(template_name='index.html')))
     urlpatterns.extend(static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT))
